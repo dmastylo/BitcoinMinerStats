b0e568835e818535c05f776be20e40da0befdaa4
Damian Mastylo <dmastylo@codequarry.net>
2013-09-14 12:29:23 -0700

    Fix typo

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -18,7 +18,7 @@ class StatsController < ApplicationController
     json_data = open('https://data.mtgox.com/api/2/BTCUSD/money/ticker', {:ssl_ca_cert => nil, :ssl_verify_mode => false}).read
     current_bitcoin_rate = JSON.parse(json_data)['data']['avg']['value']
 
-    Stat.create(current_hash_rate: current_hash_rate, current_bitcoin_rate: current_bitcoin_rate.to_f)
+    Stat.create(current_hash_rate: current_hash_rate, current_bitcoin_price: current_bitcoin_rate.to_f)
 
     flash.now[:success] = "Added stat!"
   end
