0d96d98520f710730d72b085b1b9f361dc3f5d19
Damian Mastylo <dmastylo@codequarry.net>
2013-09-14 14:49:57 -0700

    Change the parameters to our data

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -14,10 +14,8 @@ class Stat < ActiveRecord::Base
   attr_accessible :current_bitcoin_price, :current_hash_rate
 
   def self.hash_rate_stats
-    time = -5
     Stat.all.collect do |stat|
-      time = time + 5
-      [time, stat.current_hash_rate] 
+      [stat.current_hash_rate] 
     end
   end
 
