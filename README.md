576261bb1c4d994c3b6310a1358c3229fead943a
Damian Mastylo <dmastylo@codequarry.net>
2013-09-14 10:27:45 -0700

    Bitcoin price checking

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ What is it?
 A collection of Bash, Python, Perl, and Ruby on Rails code to create AWS EC2 GPU clusters
 to mine bitcoins using free AWS credits. We do all the heavy lifting, just run the script
 to automatically install EC2 instances, necessary drivers, and software and
-watch as the money rolls in!
+watch as the money rolls in! Then, visit the dashboard to see realtime updates on your
+miner and how much money is being made, and the current USD/BTC exchange rate.
--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class StatsController < ApplicationController
 
   def index
@@ -6,9 +9,12 @@ class StatsController < ApplicationController
 
   def create
     current_hash_rate = params[:avg]
-    Stat.create(current_hash_rate: current_hash_rate)
 
-    # TODO: Add bitcoin price checking
+    json_data = open('https://data.mtgox.com/api/2/BTCUSD/money/ticker', {:ssl_ca_cert => nil, :ssl_verify_mode => false}).read
+    data = JSON.parse(json_data)['avg']
+    current_bitcoin_rate = data['value']
+
+    Stat.create(current_hash_rate: current_hash_rate, current_bitcoin_rate: current_bitcoin_rate)
 
     flash.now[:success] = "Added stat!"
   end
